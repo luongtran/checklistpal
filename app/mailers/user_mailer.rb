64ab4dc491e253a20674de60f01a4d8e17b752b8
Nguyễn <thanhquang1988@gmail.com>
2013-07-13 22:58:20 +0700

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,27 +3,27 @@ class UserMailer < ActionMailer::Base
   
   def expire_email(user)
     @user = user
-    @template = EmailTemplate.find(:first , :condition => ["email_type = ?","Expire Email"])
+    @template = EmailTemplate.find(:first , :conditions => ["email_type = ?","Expire Email"])
     mail(:to => user.email, :subject => @template.title)
   end
   def thanks_email(user)
     @user = user
-    @template = EmailTemplate.find(:first , :condition => ["email_type = ?","Thanks Email"])
+    @template = EmailTemplate.find(:first , :conditions => ["email_type = ?","Thanks Email"])
     mail(:to => @user.email, :subject => @template.title)
   end
   def delete_account(user)
     @user = user
-    @template = EmailTemplate.find(:first , :condition => ["email_type = ?","Delete Account Email"])
+    @template = EmailTemplate.find(:first , :conditions => ["email_type = ?","Delete Account Email"])
     mail(:to => @user.email, :subject => @template.title)
   end
   def upgraded(user)
     @user = user
-    @template = EmailTemplate.find(:first , :condition => ["email_type = ?","Upgraded Email"])
+    @template = EmailTemplate.find(:first , :conditions => ["email_type = ?","Upgraded Email"])
     mail(:to => @user.email , :subject => @template.title)
   end
   def downgraded(user)
     @user = user
-    @template = EmailTemplate.find(:first , :condition => ["email_type = ?","Downgraded Email"])
+    @template = EmailTemplate.find(:first , :conditions => ["email_type = ?","Downgraded Email"])
     mail(:to => @user.email , :subject => @template.title)    
   end
 end
