2f3b8260f8b60902151cfef83cef08c20c019ca5
Luanht <htluan2605@gmail.com>
2013-07-27 08:45:53 +0700

    abc

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,5 +1,5 @@
 //= require active_admin/base
-//= require_self
-//= require ckeditor/ckeditor
-//= require ckeditor/config
+// require_self
+// require ckeditor/ckeditor
+// require ckeditor/config
 var CKEDITOR_BASEPATH = '/assets/ckeditor/';
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,16 +1,16 @@
 class ListsController < ApplicationController
-  before_filter :authenticate_user!, :only => [:destroy ,:mylist, :edit]
+  before_filter :authenticate_user!, :only => [:destroy, :mylist, :edit]
   respond_to :html, :xml, :js
- 
+
   def show
-    @list = List.find(:first , :conditions => ["slug = ?", params[:slug]])
+    @list = List.find(:first, :conditions => ["slug = ?", params[:slug]])
     if !@list.blank?
-      if current_user.present?   # if user login
-        if @list.user_id.present?        # if list not have user_id < public list
-          if current_user.id != @list.user_id   # if current_user is not owner
+      if current_user.present? # if user login
+        if @list.user_id.present? # if list not have user_id < public list
+          if current_user.id != @list.user_id # if current_user is not owner
             if @list.list_team_members.present?
-              @list_team_member = @list.list_team_members.find(:first , :conditions => ['invited_id = ? AND list_id =? ', current_user.id ,@list.id ])
-              if !@list_team_member.blank?                    
+              @list_team_member = @list.list_team_members.find(:first, :conditions => ['invited_id = ? AND list_id =? ', current_user.id, @list.id])
+              if !@list_team_member.blank?
                 @owner = User.find(@list.user_id)
                 flash[:notice] = "You are viewing list of #{@owner.email}"
                 return
@@ -25,11 +25,11 @@ class ListsController < ApplicationController
         else
           return
         end
-      else     #if user not login
-        if @list.user_id.present?   # if list  have user_id < Private list
+      else #if user not login
+        if @list.user_id.present? # if list  have user_id < Private list
           flash[:alert] = "You have no permission to view this list !"
           redirect_to root_path
-        else                         # if list not have user_id < Public list
+        else # if list not have user_id < Public list
           return
         end
       end
@@ -37,35 +37,36 @@ class ListsController < ApplicationController
       flash[:notice] = "List not exists or deleted !"
       redirect_to root_path
     end
-    
+
   end
 
   def edit
-    if User.list_create(current_user.id,params[:id])    
+    if User.list_create(current_user.id, params[:id])
       @permission = true
       @list = List.find(params[:id])
       if @list.update_attributes(params[:list])
         @success = 1
       else
         @success = 0
-      end        
-    else    
+      end
+    else
       @permission = false
     end
     respond_to do |format|
-      format.json { render :json => {:success => @success, :list => @list , :permission => @permission}}   
+      format.json { render :json => {:success => @success, :list => @list, :permission => @permission} }
     end
   end
-  
+
   def destroy
     @list = List.find(params[:id])
-    if current_user.id == @list.user_id 
+    if current_user.id == @list.user_id
       @list.destroy
       redirect_to my_list_url notice: "List Deleted"
     else
-      redirect_to root_path  alert: "Can't delete list"
+      redirect_to root_path alert: "Can't delete list"
     end
   end
+
   def update
     @list = List.find(params[:id])
     if @list.update_attributes(params[:list])
@@ -75,43 +76,43 @@ class ListsController < ApplicationController
       redirect_to edit_list_path(@list)
     end
   end
-  
+
   def mylist
     @user = current_user
-    @lists = List.find(:all, :conditions => ["user_id = ?" ,@user.id])
-    @list_team_members = ListTeamMember.find(:all, :conditions => ["invited_id = ? AND active = ?", @user.id , true])
+    @lists = List.find(:all, :conditions => ["user_id = ?", @user.id])
+    @list_team_members = ListTeamMember.find(:all, :conditions => ["invited_id = ? AND active = ?", @user.id, true])
     list_ids = []
     if @list_team_members
       @list_team_members.each do |member|
         list_ids += [member.list_id]
       end
       if !list_ids.empty?
-        @friend_lists = List.where('id IN (?)',list_ids)	  	
+        @friend_lists = List.where('id IN (?)', list_ids)
       end
     end
-    @list_team_members2 = current_user.list_team_members.find(:all , :conditions =>["active = ?", true])   
+    @list_team_members2 = current_user.list_team_members.find(:all, :conditions => ["active = ?", true])
     user_ids = []
     if @list_team_members2
       @list_team_members2.each do |member|
         user_ids += [member.invited_id]
       end
       if !user_ids.empty?
-        @my_connects = User.where('id IN (?)',user_ids)
+        @my_connects = User.where('id IN (?)', user_ids)
       end
-    end 
+    end
     response do |format|
       format.html
     end
   end
-  
+
   def remove_connect
     @is_not_connect = false
     @list_id = params[:list_id]
     @user_id = params[:user_id]
-    @current_connect = ListTeamMember.find(:all, :conditions => ["list_id = ? and invited_id = ?",@list_id,@user_id])
+    @current_connect = ListTeamMember.find(:all, :conditions => ["list_id = ? and invited_id = ?", @list_id, @user_id])
     if @current_connect.empty?
       @success = false
-    else 
+    else
       @current_connect.each do |f|
         f.destroy
       end
@@ -120,11 +121,11 @@ class ListsController < ApplicationController
       redirect_to my_list_url
     end
   end
-  
+
   def search_my_list
     @user = current_user
     @list_name = params[:list_name]
-    @lists = List.find(:all, :conditions => ["user_id = ? AND name like ?" ,@user.id,"%#{@list_name}%"])
+    @lists = List.find(:all, :conditions => ["user_id = ? AND name like ?", @user.id, "%#{@list_name}%"])
     if @lists.length > 0
       @success = true
     else
