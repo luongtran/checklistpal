445e37b64fbd701b21766a7bcacd0b45c07e9957
Luanht <htluan2605@gmail.com>
2013-07-27 16:13:26 +0700

    some commit

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -34,6 +34,7 @@ class UserMailer < ActionMailer::Base
     @template = EmailTemplate.find(:first, :conditions => ["email_type = ?","Welcome Email"])
     mail(:to => @user.email , :subject => @template.title)
   end
+  ### Test
   def self.test_send
     mail(:to => "htluan2605@gmail.com" , :subject => "Hello") 
   end
