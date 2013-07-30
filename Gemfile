d71c0140d6cd4a1b3b6357fb38b3155750ac9eb9
Luanht <htluan2605@gmail.com>
2013-07-30 11:21:41 +0700

    uname commit

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
-gem 'mysql2'
 
 gem 'pg'
 #gem 'ambethia-smtp-tls'
--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,7 @@ class List < ActiveRecord::Base
   has_many :tasks ,  :dependent => :delete_all
   has_many :list_team_members,:dependent => :delete_all
   validates_uniqueness_of :slug
+  validates_presence_of :name
 #  def to_param
 #    slug
 #  end
