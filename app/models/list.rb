62d1a80d5456253f0b0e5b972af74b6aafa07e69
Luanht <htluan2605@gmail.com>
2013-07-30 11:24:45 +0700

    asd

--- app/models/list.rb
+++ app/models/list.rb
@@ -6,7 +6,5 @@ class List < ActiveRecord::Base
   has_many :list_team_members,:dependent => :delete_all
   validates_uniqueness_of :slug
   validates_presence_of :name
-#  def to_param
-#    slug
-#  end
+
 end
