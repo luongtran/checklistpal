365ce426214eadada2c121fa619bfd7161f7bc5a
Luanht <htluan2605@gmail.com>
2013-08-19 16:20:08 +0700

    update 'after update path'

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -75,6 +75,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
   def after_update_path_for(resource)
-    edit_user_registration_path
+    my_account_path
   end
 end
